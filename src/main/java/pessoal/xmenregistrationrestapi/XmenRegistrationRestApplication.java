b732e9ebdba9db2379500b6009ae8919102d22ca
JulianaPerfeito <julianaperfeito@usp.br>
2022-01-29 18:58:34 -0300

    Iniciando o projeto Desenvolvendo um sistemas de gerenciamento de pessoas em API REST com Spring Boot do Bootcamp Amdocs JAVA Developer

---
+++ src/main/java/pessoal/xmenregistrationrestapi/XmenRegistrationRestApplication.java
@@ -0,0 +1,13 @@
+package pessoal.xmenregistrationrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmenRegistrationRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmenRegistrationRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pessoal/xmenregistrationrestapi/XmenRegistrationRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pessoal.xmenregistrationrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmenRegistrationRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
