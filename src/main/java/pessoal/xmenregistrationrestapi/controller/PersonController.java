570072738ff69bd02df3f19e034f3345ac28c1c7
JulianaPerfeito <julianaperfeito@usp.br>
2022-01-30 22:09:04 -0300

    Criação de um README.md

--- src/main/java/pessoal/xmenregistrationrestapi/controller/PersonController.java
+++ src/main/java/pessoal/xmenregistrationrestapi/controller/PersonController.java
@@ -1,8 +1,17 @@
 package pessoal.xmenregistrationrestapi.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import pessoal.xmenregistrationrestapi.dto.request.PersonDTO;
 import pessoal.xmenregistrationrestapi.dto.response.MessageResponseDTO;
 import pessoal.xmenregistrationrestapi.exceptions.PersonNotFoundException;
@@ -13,14 +22,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
